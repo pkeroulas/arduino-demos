5dd5acb37c032946a0b71739480ce5cc7017e27c
Patrick Keroulas <patrick.keroulas@gmail.com>
2021-09-19 11:43:55 -0400

    neopixel: update test batch

--- neopix/serial/batch.sh
+++ neopix/serial/batch.sh
@@ -18,86 +18,102 @@ shoot() {
 #shoot 1 1 1 0.1
 #shoot 1 1 100 0.1
 #shoot 1 1 255 0.01
+
 reset(){
     python arduino_client.py 0 0 0 0 0 0
-    sleep 1
 }
 wave(){
     echo wave
-    python arduino_client.py 0 1 0 0 100 0
-    sleep 1
+    python arduino_client.py 0 1 0 0 0 0
 }
 fire(){
     echo fire
     python arduino_client.py 0 2 50 50 0 0
-    sleep 1
-}
-flash(){
-    python arduino_client.py 0 4 100 100 100 0
-    sleep 1
 }
 meteor(){
-    echo fire
-    python arduino_client.py 0 3 0 100 0 0
-    sleep 1
+    echo meteor
+    python arduino_client.py 0 3 0 0 0 0
+}
+flash(){
+    echo flash
+    python arduino_client.py 0 4 100 100 100 0
+}
+fill(){
+    echo fill
+    python arduino_client.py 0 5 10 10 10 0
+}
+purple(){
+    echo fill
+    python arduino_client.py 0 6 50 0 100 0
+}
+cyan(){
+    echo fill
+    python arduino_client.py 0 6 0 80 80 0
+}
+orange(){
+    echo fill
+    python arduino_client.py 0 6 80 80 0 0
+}
+pause(){
+    echo "........ pause... press key"
+    read whatev
 }
 
-reset
+sequence() {
+    reset
 
-echo "--------------"
-wave
-flash
-flash
-reset
+    #echo "--------------"
+    #wave
+    #flash
+    #flash
+    #reset
 
-sleep 1
+    sleep 1
+    pause
 
-echo "--------------"
-fire
-flash
-flash
-reset
+    fire
+    flash
+    flash
+    reset
 
-sleep 1
+    pause
 
-echo "--------------"
-meteor
-flash
-flash
-reset
+    meteor
+    flash
+    flash
+    reset
 
-sleep 1
+    pause
 
-echo "--------------"
-wave
-sleep 1
-fire
-sleep 1
-meteor
-sleep 1
+    wave
+    pause
+    fire
+    sleep 1
+    meteor
 
-echo "--------------"
-fire
-sleep 1
-meteor
-sleep 1
-wave
-sleep 1
+    pause
 
-echo "--------------"
-meteor
-sleep 1
-wave
-sleep 1
-fire
-sleep 1
+    fire
+    sleep 1
+    meteor
+    sleep 1
+    wave
+    pause
 
-echo "--------------"
-wave
-sleep 1
-meteor
-sleep 1
-fire
-sleep 1
+    meteor
+    sleep 1
+    wave
+    sleep 1
+    fire
+    pause
 
-python arduino_client.py "kill"
+    wave
+    pause
+    meteor
+    sleep 1
+    fire
+
+    pause
+
+    python arduino_client.py "kill"
+}
